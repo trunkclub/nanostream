c56407181255f5fe4ee5b7c340d8990fccfb0a59
Zachary Ernst <zac.ernst@gmail.com>
2016-11-18 14:12:54 -0600

    Fixed networkx requirement

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 
 setup(
     name='nanostream',
-    version='0.1-alpha12',
+    version='0.1-alpha13',
     description='Stream-processing patterns at small scale, no overhead',
     long_description=(
         'Set up asychronous stream processing using pure Python with minimal '
@@ -21,5 +21,5 @@ setup(
     packages=['nanostream'],
     package_dir={'nanostream': 'nanostream'},
 
-    install_requires=['pyprimes', 'networkx>1.11,<2.0']
+    install_requires=['pyprimes', 'networkx>=1.11,<2.0']
 )
